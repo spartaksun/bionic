033eb887d6d4b71ed843cb41d10d48f6dfaa0a52
Alexey Belyakovskiy <alexeyb@compandsave.com>
2017-08-10 14:40:55 +0300

    Homework 4

---
+++ hw4/webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'jshint-loader'
+			}
+		]
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: './src/index.html' }
+		])
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist')
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
